e8a8f90d71df7d33f533baf9f090af51c7914194
Chris <cf_anderson@netzero.com>
2019-07-03 07:58:42 -0500

    added another class

--- Week3Project/Week3Project/Program.cs
+++ Week3Project/Week3Project/Program.cs
@@ -8,21 +8,16 @@ namespace Week3Project
         {
             Console.WriteLine("What's your first name?");
             string response1 = Console.ReadLine();
+
             Console.WriteLine("What's your last name?");
             string response2 = Console.ReadLine();
+
             Console.WriteLine("What's your favorite number?");
             string response3 = Console.ReadLine();
+
             Console.WriteLine("How many pets do you have?");
-            string input = Console.ReadLine();
-            int num = -1;
-            if (!int.TryParse(input, out num))
-            {
-                Console.WriteLine("Not an integer");
-            }
-            else
-            {
-                
-           }
+            string response4 = Console.ReadLine();
+            
             Console.WriteLine("What are the names of your pets?");
             string response5 = Console.ReadLine();
 
